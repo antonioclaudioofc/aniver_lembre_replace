01c8674e63f7a25192e69d32f4a57809b62c09fa
Antonio Claudio <claudioalvesdev@gmail.com>
2025-12-10 14:31:18 -0300

    feat: create model user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(default=datetime.now())
