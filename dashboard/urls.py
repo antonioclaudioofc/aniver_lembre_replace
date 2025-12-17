76e36ecad0a015f60b52aafe4f534bd0abc92f32
Antonio Claudio <claudioalvesdev@gmail.com>
2025-12-17 08:41:34 -0300

    feat: update forms and templates for reminders, add delete functionality

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,6 @@ app_name = 'dashboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-reminder/', views.create_reminder, name='create_reminder'),
+    path('reminder/<int:reminder_id>/delete/', views.delete_reminder,
+         name='delete_reminder'),
 ]
